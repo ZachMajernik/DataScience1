efeafa41c06d1e375f864c3af881d013a43081fe
ZachMajernik <zachmajernik@gmail.com>
2023-11-13 11:33:38 -0700

    js notes

---
+++ CodeTemplates/JS/DemoScript.js
@@ -0,0 +1,9 @@
+// JavaScript source code
+window.onload = function () {
+
+    var back = document.getElementById("animBackground");
+    back.style.width = 0;
+
+    var text = document.getElementById("animText");
+    text.style.opacity = 1;
+}
