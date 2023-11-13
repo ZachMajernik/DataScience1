20540cfe8faf419f2e60f1e9ee4675c554bee40f
ZachMajernik <zachmajernik@gmail.com>
2023-11-13 10:29:51 -0700

    animation on page load

--- CodeTemplates/JS/script.js
+++ CodeTemplates/JS/script.js
@@ -1,19 +1,16 @@
 // JavaScript source code
-
 window.onload = function () {
-    // Select the element to animate
+
     var left = document.getElementsByClassName("left");
 
-    // Apply the animation by changing the element's style
     for (var i = 0; i < left.length; i++) {
         left[i].style.width = 0;
     }
 
-    /*var right = document.getElementsByClassName("right");
+    var right = document.getElementsByClassName("right");
 
-    // Apply the animation by changing the element's style
     for (var i = 0; i < right.length; i++) {
         right[i].style.width = 0;
-    }*/
+    }
     
 };
